abcf445969eb2077565a82061ccee40db2df30f5
Larysa <goroshanska@gmail.com>
2017-05-30 00:58:19 +0300

    Add html for new website

---
+++ projects/new/app/js/works.js
@@ -0,0 +1,93 @@
+(function () {
+  'use strict';
+
+  // If there isn't names array yet
+  if (localStorage.getItem('result') === null) {
+    // Take names from public API
+    requestWorks();
+  } else {
+    // Take works from localStorage
+    let result = localStorage.result ? JSON.parse(localStorage.result) : [];
+    showWorks(result);
+  }
+
+
+  function requestWorks() {
+
+    // Create url for request
+    let url = 'https://api.myjson.com/bins/1gg5n5';
+
+    // Create new object XMLHttpRequest
+    let xhr = new XMLHttpRequest();
+    // Confige object XMLHttpRequest
+    xhr.open('GET', url, true);
+    xhr.onreadystatechange = receiveResponse;
+    xhr.send();
+
+
+    function receiveResponse(e) {
+      if (this.readyState == 4) {
+        // xhr.readyState == 4, so we've received the complete server response
+        if (this.status == 200) { // xhr.status == 200, so the response is good
+
+          var result = JSON.parse(xhr.responseText);
+          // Write names array into localStorage
+          localStorage.result = JSON.stringify(result);
+        } else {
+          ups();
+        }
+      }
+    }
+  }
+
+  function showWorks(result) {
+    let container = document.getElementById('container');
+    //container.innerHTML = result.works[0].title;
+
+    for (let i = 0; i < result.works.length; i++) {
+      let link = document.createElement('a');
+      link.classList = 'card-link';
+      link.setAttribute('target', '_blank');
+      link.setAttribute('href', result.works[i].link);
+
+      let card = document.createElement('div');
+      card.classList = 'card';
+
+      let divImg = document.createElement('div');
+      let image = document.createElement('img');
+
+      image.classList = 'card__img';
+      image.setAttribute('data-rjs', '2');
+      image.setAttribute('src', result.works[i].image);
+
+      let shadow = document.createElement('div');
+      shadow.classList = 'card__shadow';
+
+      let title = document.createElement('h3');
+      title.classList = 'card__title';
+      title.innerHTML = result.works[i].title;
+
+      let text = document.createElement('p');
+      text.classList = 'card__text';
+      text.innerHTML = result.works[i].description;
+
+      shadow.appendChild(title);
+      shadow.appendChild(text);
+
+      divImg.appendChild(image);
+      divImg.appendChild(shadow);
+      link.appendChild(divImg);
+      container.appendChild(link);
+    }
+  }
+
+  function ups() {
+    let container = document.getElementById('container');
+    let p = document.createElement('p');
+    p.classList = 'works__text';
+    p.innerHTML = 'Sorry, works aren\'t available at the moment.';
+    container.appendChild(p);
+  }
+
+
+})();
