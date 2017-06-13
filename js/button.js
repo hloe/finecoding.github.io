c4cbd23d978cfe1791eb4cc3890fb3cc425a66d2
Larysa <goroshanska@gmail.com>
2017-06-13 19:01:09 +0300

    Add new design

--- js/button.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-let button = document.getElementsByClassName('purple-button')[1];
-
-button.addEventListener('click', go);
-
-function go(event) {
-  event.preventDefault;
-  button.innerHTML = (button.innerHTML === 'view more') ? 'view less' : 'view more';
-}
